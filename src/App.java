7d6a903e73d76b031247ab04546da55150a048a5
dmanikyam-ATL <52053184+dmanikyam-ATL@users.noreply.github.com>
2023-01-27 17:29:49 +0000

    Working with Array Data

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 public class App {
   
   public static void main(String[] args) {
-    
+    double[] lnumbers= {45,92,38,33,21};
+    System.out.println(lnumbers[2]);
+    lnumbers[2]=40;
+    System.out.println(lnumbers[2]);
   }
 }
